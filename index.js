141f62e830804a4c1526d155f7a02fc34f9703aa
adarsh-av13 <dra4474@gmail.com>
2019-10-17 21:54:49 +0530

    added sound effects

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const cards = Array.from(document.querySelectorAll('.cards'));
 const startButton = document.querySelector('button');
 const timeDis = document.querySelector('.time');
 let score = 0;
+const click = document.querySelector('.click');
+const wrong = document.querySelector('.wrong');
+const correct = document.querySelector('.correct');
 
 const cpics = [ 
     'crookshanks.png',
@@ -23,9 +26,18 @@ let isClicked = [
                 false,false,false,false,
                 false,false,false,false
             ];
+let isDone = [
+                false,false,false,false,
+                false,false,false,false,
+                false,false,false,false,
+                false,false,false,false
+            ];
+
 let isHalf = false,lastClicked;
 let curTime = new Date();
+let myTimer;
 
+let toggled = false;
 
 function flip(){
     let x = this.id;
@@ -37,34 +49,40 @@ function flip(){
     if(!isClicked[y]){
       front.style.display = 'inline-block';
       back.style.display = 'none';
+      click.play();
       if(lastClicked!=y)
         isHalf = !isHalf;
       if(!isHalf){
         if(y!=lastClicked && crd[y]==crd[lastClicked]){
-          setTimeout(function(){ 
+          setTimeout(function(){
+            correct.play();
             front.src = 'Images/black.jpg';
             back.src = 'Images/black.jpg';
             cards[y].removeEventListener('dblclick',flip);
+            isDone[y]  =true;
             front = cards[lastClicked].querySelector('.front');
             back =  cards[lastClicked].querySelector('.back');
             front.src = 'Images/black.jpg';
             back.src = 'Images/black.jpg';
             cards[lastClicked].removeEventListener('dblclick',flip);
+            isDone[lastClicked] = true;
             lastClicked = -1;
             score++;
           },200);  
         }
         else {
           setTimeout(function() {
+            wrong.play();
             front.style.display = 'none';
             back.style.display = 'block';
             front = cards[lastClicked].querySelector('.front');
             back =  cards[lastClicked].querySelector('.back');
-            console.log(lastClicked,front,back);
+            isClicked[lastClicked] = false;
+            isClicked[y] = false;
             front.style.display = 'none';
             back.style.display = 'block';
             lastClicked = -1;
-          },200);  
+          },800);  
         }
       }
       else{
@@ -74,14 +92,34 @@ function flip(){
     else{
       front.style.display = 'none';
       back.style.display = 'block';  
+      click.play();
     }
     if(score==8)
       gameOver = true;
     isClicked[y] = !isClicked[y];
-
 }
 
 function reduceTime(){
+    gameOver = true;
+    for(let i=0;i<16;++i){
+      if(isDone[i]===false){
+        gameOver = false;
+        break;
+      }
+    }  
+
+    if(gameOver == true){
+      clearInterval(myTimer);
+      let audio = document.querySelector('.winner');
+      audio.play(); 
+      for(let i=0;i<8;++i){
+        let front = cards[i].querySelector('.front');
+        let back =  cards[i].querySelector('.back');
+        front.src = `Images/winner${i}.jpg`;
+        back.src = `Images/winner${i}.jpg`;
+      }
+      return;
+    }  
     let secs = curTime.getSeconds();
     let mins = curTime.getMinutes();
     if(mins==0 && (secs-1) == -1){
@@ -96,13 +134,12 @@ function reduceTime(){
     else{
         secs--;
     }
-    
     curTime.setSeconds(secs);
     curTime.setMinutes(mins); 
     if(curTime.getSeconds()<10)
       x=0;
     else 
-      x='';    
+      x='';
     timeDis.innerHTML = '0'+curTime.getMinutes()+':'+x+curTime.getSeconds();  
 }
 
@@ -117,10 +154,11 @@ function startGame(){
     curTime.setSeconds(5);
     timeDis.innerHTML = '0'+curTime.getMinutes()+':'+'0'+curTime.getSeconds();
     cards.forEach(card => card.addEventListener('dblclick',flip)); 
-    setInterval(reduceTime,1000);         
+    myTimer = setInterval(reduceTime,1000);         
 }
 cards.forEach(card => {
     let front = card.querySelector('.front');
     front.src = `Images/${cpics[crd[cards.indexOf(card)]]}`;
 });
+
 startButton.addEventListener('click',startGame);
